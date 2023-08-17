2a24097c5a010692c45905ff8388ac071b0e7643
M Naeem Akram <naeemakram884@gmail.com>
2023-08-18 00:37:20 +0500

    Done Designation

--- app/Http/Controllers/Admin/DesignationController.php
+++ app/Http/Controllers/Admin/DesignationController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Department;
 use App\Models\Designation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class DesignationController extends Controller
 {
@@ -35,33 +36,61 @@ class DesignationController extends Controller
         return view('admin.company-hq.designation.index', $viewParams);
     }
 
-    public function create()
-    {
-        //
-    }
-
     public function store(Request $request)
     {
-        //
-    }
+        $validatedData = $request->validate([
+            'department_id' => 'required|not_in:0',
+            'name' => 'required|unique:designations',
+        ]);
 
-    public function show(Designation $designation)
-    {
-        //
+        $designation = new Designation;
+        $designation->department()->associate($validatedData['department_id']);
+        $designation->name = $request->name;
+        $designation->status = ($request->status == "on") ? 1 : 0;
+
+        if ($designation->save()) {
+            Session::flash('successMessage', 'A new designation added successfully!');
+            return redirect()->back();
+        }
+
+        return redirect()->back()
+            ->withErrors('Failed to create new designation, Try again!');
     }
 
     public function edit(Designation $designation)
     {
-        //
+        return response()->json($designation);
     }
 
     public function update(Request $request, Designation $designation)
     {
-        //
+        $validatedData = $request->validate([
+            'edit_department_id' => 'required|not_in:0',
+            'edit_name' => 'required|unique:designations,name,' . $designation->id,
+        ]);
+
+        if ($designation) {
+            $designation->department()->associate($validatedData['edit_department_id']);
+            $designation->name = $validatedData['edit_name'];
+            $designation->status = ($request->edit_status == "on") ? 1 : 0;
+            $designation->save();
+
+            Session::flash('successMessage', 'Designation updated successfully!');
+            return redirect()->back();
+        }
+
+        return redirect()->back()
+            ->withErrors('Invalid data, Try again!');
     }
 
     public function destroy(Designation $designation)
     {
-        //
+        if ($designation->delete()) {
+            Session::flash('successMessage', 'Designation deleted successfully!');
+            return redirect()->back();
+        }
+
+        return redirect()->back()
+            ->withErrors('Failed to delete designation, Try again!');
     }
 }
