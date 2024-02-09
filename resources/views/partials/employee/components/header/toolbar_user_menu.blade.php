42fe2af5a2b3f0f3f36ce451d2af7c0ec1f9d09e
naeemakram4 <naeemakram4@hotmail.com>
2024-02-10 00:43:26 +0500

    Updated employee header user panel for role label display

--- resources/views/partials/employee/components/header/toolbar_user_menu.blade.php
+++ resources/views/partials/employee/components/header/toolbar_user_menu.blade.php
@@ -16,8 +16,9 @@
                 <!--end::Avatar-->
                 <!--begin::Username-->
                 <div class="d-flex flex-column">
-                    <div class="fw-bolder d-flex align-items-center fs-5">{{ auth()->user()->name }}
-                        <span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2">Customer</span></div>
+                    <div class="fw-bolder d-flex align-items-center fs-6">{{ auth()->user()->getFullName() }}
+                        <span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2">{{ auth()->user()->role->label }}</span>
+                    </div>
                     <a href="#" class="fw-bold text-muted text-hover-primary fs-7">{{ auth()->user()->email }}</a>
                 </div>
                 <!--end::Username-->
