19e8be407d33191a257f28d53f2d24cf9a8281e9
naeemakram4 <naeemakram4@hotmail.com>
2024-02-10 10:52:46 +0500

    Updated customer client issue

--- resources/views/employee/client/components/table.blade.php
+++ resources/views/employee/client/components/table.blade.php
@@ -14,20 +14,20 @@
     @foreach($clients as $client)
         <tr>
             <td>{{ $client->user->first_name .' '. $client->user->last_name }}</td>
-            <td>{{ $client->company_name }} <br>
-                <a href="{{ $client->website }}" target="_blank">{{ $client->website }}</a>
+            <td>{{ $client->company_name ?? ''}} <br>
+                <a href="{{ $client->website ?? '' }}" target="_blank">{{ $client->website ?? ''}}</a>
             </td>
             <td>
                 <a href="tel:{{ $client->user->phone }}">{{ $client->user->phone }}</a> <br>
                 <a href="mailto:{{ $client->user->email }}">{{ $client->user->email }}</a></td>
             <td>
                 @foreach($client->services as $service)
-                    <span class="badge badge-light-dark">{{ $service->label }}</span>
+                    <span class="badge badge-light-dark">{{ $service->label ?? ''}}</span>
                 @endforeach
             </td>
-            <td>{{ $client->start_date }}</td>
+            <td>{{ $client->start_date ?? ''}}</td>
             <td>
-                <span class="badge badge-light-primary">{{ $client->clientType->label }}</span>
+                <span class="badge badge-light-primary">{{ $client->clientType->label ?? ''}}</span>
             </td>
             <td>
                 @if($client->user->status == \App\Models\User::STATUS_ACTIVE)
