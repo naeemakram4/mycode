e858b4724a54b62d0c6f0b0276a1998c6f7cc7d8
naeemakram4 <naeemakram4@hotmail.com>
2024-04-06 01:31:26 +0500

    Customer portal, removed the project tiles form dashboard

--- resources/views/customer/dashboard/components/row1.blade.php
+++ resources/views/customer/dashboard/components/row1.blade.php
@@ -1,53 +1,6 @@
 <div class="row g-5 g-xl-8">
     <div class="col-xl-3">
         <!--begin::Statistics Widget 5-->
-        <a href="javascript:void(0);" class="card bg-primary mb-xl-8">
-            <!--begin::Body-->
-            <div class="card-body">
-                <div class="d-flex flex-stack justify-end flex-grow-1">
-                    <!--begin::Svg Icon | path: icons/duotune/general/gen032.svg-->
-                    <span class="svg-icon svg-icon-light svg-icon-3x ms-n1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-kanban text-light" viewBox="0 0 16 16">
-                      <path d="M13.5 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-11a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zm-11-1a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-                      <path d="M6.5 3a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1zm-4 0a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1zm8 0a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1z"/>
-                    </svg>
-                </span>
-                    <!--end::Svg Icon-->
-                    <div>
-                        <div id="TotalActiveProjects" class="text-light fw-bolder fs-2 mb-2">1</div>
-                        <div class="fw-bold text-light fs-3">Active Projects</div>
-                    </div>
-                </div>
-            </div>
-            <!--end::Body-->
-        </a>
-        <!--end::Statistics Widget 5-->
-    </div>
-    <div class="col-xl-3">
-        <!--begin::Statistics Widget 5-->
-        <a href="javascript:void(0);" class="card bg-warning mb-xl-8">
-            <!--begin::Body-->
-            <div class="card-body">
-                <div class="d-flex flex-stack justify-end flex-grow-1">
-                    <!--begin::Svg Icon | path: icons/duotune/general/gen032.svg-->
-                    <span class="svg-icon svg-icon-light svg-icon-3x ms-n1">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-kanban-fill text-light" viewBox="0 0 16 16">
-                      <path d="M2.5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h11a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm5 2h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1m-5 1a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1zm9-1h1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1"/>
-                    </svg>
-                </span>
-                    <!--end::Svg Icon-->
-                    <div>
-                        <div id="TotalPendingProjects" class="text-light fw-bolder fs-2 mb-2">2</div>
-                        <div class="fw-bold text-light fs-3">Pending Projects</div>
-                    </div>
-                </div>
-            </div>
-            <!--end::Body-->
-        </a>
-        <!--end::Statistics Widget 5-->
-    </div>
-    <div class="col-xl-3">
-        <!--begin::Statistics Widget 5-->
         <a href="javascript:void(0);" class="card bg-success mb-xl-8">
             <!--begin::Body-->
             <div class="card-body">
